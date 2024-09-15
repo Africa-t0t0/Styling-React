c2000a8193c54d2be8020923fdf3b322d5846b78
Africa-t0t0 <jorge.ramirezz@mail.udp.cl>
2024-09-15 18:20:46 -0300

    refactor project to use tailwind

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -5,12 +5,13 @@ import Button from './Button';
 import Input from './Input';
 
 
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
+// const ControlContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 0.5rem;
+//   margin-bottom: 1.5rem;
+// `
+
 
 
 
@@ -37,9 +38,14 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
+    <div
+      id="auth-inputs"
+      className='w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800'
+    >
       {/* <div className="controls"> */}
-      <ControlContainer>
+      <div
+        className='flex flex-col gap-2 mb-6'
+      >
         {/* <p> */}
             {/* <Label
               $invalid={emailNotValid}
@@ -89,14 +95,15 @@ export default function AuthInputs() {
             /> */}
           {/* </p> */}
 
-      </ControlContainer>
+      </div>
       {/* </div> */}
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div
+        className="flex justify-end gap-4"
+        >
+        <button type="button" className='text-amber-400 hover:text-amber-500'>
           Create a new account
         </button>
         <Button
-          className='button'
           onClick={handleLogin}
         >
           Sign In
